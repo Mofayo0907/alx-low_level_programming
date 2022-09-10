cede92cc1e8264fb62610045c0395c7bd6896b9f
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-10 02:33:53 +0100

    102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,90 +1,31 @@
 #include <stdio.h>
-
-#include <stdlib.h>
-
 /**
- * main - main block
- * Description: Write a program that prints all possible
- * different combinations of two digits
- * Return: 0
- */
-
+* main-print all possible combinations of two two digits
+* Return: 0 if successful
+*/
 int main(void)
-
 {
+int i, j;
 
-int c;
-
-int d;
-
-int e;
-
-int f = 0;
-
-while (f < 10)
-
+for (i = 0; i < 100; i++)
 {
-
-	e = 0;	while (e < 10)
-
-	{
-
-		d = 0;
-
-		while (d < 10)
-
-		{
-
-			c = 0;
-
-			while (c < 10)
-
-			{
-
-				if (!(f == c && e == d))
-
-				{
-
-					putchar('0' + f);
-
-					putchar('0' + e);
-
-					putchar(' ');
-
-					putchar('0' + d);
-
-					putchar('0' + c);
-
-					if (!(f + e == 18 && c + d == 17 && d == 9))
-
-					{
-
-						putchar(',');
-
-						putchar(' ');
-
-					}
-
-				}
-
-				c++;
-
-			}
-
-			d++;
-
-		}
-
-		e++;
-
-	}
-
-	f++;
-
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
 }
-
+}
+}
+}
 putchar('\n');
-
 return (0);
-
 }
