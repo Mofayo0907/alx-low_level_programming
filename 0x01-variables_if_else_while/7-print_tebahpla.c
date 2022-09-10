2687053c95bc196b7b60248761378574005ff16f
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-10 02:13:42 +0100

    if/else

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 /**
- * * main-print the lowercase alphabet in reverse
- * * followed by a new line
- * * Return: 0 if successful
- * */
+* main-print the lowercase alphabet in reverse
+* followed by a new line
+* Return: 0 if successful
+*/
 int main(void)
 {
-	char ch;
-	for (ch = 'z'; ch >= 'a'; ch--)
-	{
-		putchar(ch);
-	}
-	putchar('\n');
-	return (0);
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+{
+	putchar(ch);
+}
+putchar('\n');
+return (0);
 }
