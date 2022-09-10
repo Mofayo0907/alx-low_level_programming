58516c6cdc1349bc9d29cc16e0796712379ad210
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-10 02:30:38 +0100

    if/else

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,88 +3,59 @@
 #include <stdlib.h>
 
 /**
- *  * main - main block
- *   * Description: Write a program that prints all possible
- *    * different combinations of two digits
- *     * Return: 0
- *      */
+ * main - main block
+ * Description: Write a program that prints all possible
+ * different combinations of two digits
+ * Return: 0
+ */
 
 int main(void)
-
 {
 
-	int c;
+int c;
 
-	int d;
+int d;
 
-	int e;
+int e;
 
-	int f = 0;
+int f = 0;
 
-	while (f < 10)
+while (f < 10)
 
-	{
+{
+e = 0;	while (e < 10)
+{
+d = 0;
+while (d < 10)
+{
+c = 0;
+while (c < 10)
+{
+if (!(f == c && e == d))
+{
+putchar('0' + f);
 
-			e = 0;	while (e < 10)
+putchar('0' + e);
+				putchar(' ');
+putchar('0' + d);
+putchar('0' + c);
+if (!(f + e == 18 && c + d == 17 && d == 9))
+{
+putchar(',');
+putchar(' ');
+}
+}
+c++;
+}
+d++;
+}
+e++;
+}
+f++;
+}
 
-					{
+putchar('\n');
 
-								d = 0;
-
-										while (d < 10)
-
-													{
-
-																	c = 0;
-
-																				while (c < 10)
-
-																								{
-
-																													if (!(f == c && e == d))
-
-																																		{
-
-																																								putchar('0' + f);
-
-																																													putchar('0' + e);
-
-																																																		putchar(' ');
-
-																																																							putchar('0' + d);
-
-																																																												putchar('0' + c);
-
-																																																																	if (!(f + e == 18 && c + d == 17 && d == 9))
-
-																																																																							{
-
-																																																																														putchar(',');
-
-																																																																																				putchar(' ');
-
-																																																																																									}
-
-																																																																					}
-
-																																	c++;
-
-																																				}
-
-																							d++;
-
-																									}
-
-												e++;
-
-													}
-
-				f++;
-
-	}
-
-	putchar('\n');
-
-	return (0);
+return (0);
 
 }
