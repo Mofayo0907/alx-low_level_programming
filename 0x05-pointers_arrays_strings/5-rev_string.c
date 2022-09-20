f64d201a7b27b82f5c45710afcc358635d17efd8
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-20 01:21:02 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,44 +1,43 @@
 #include "main.h"
 /**
- *  * rev_string-print a reverse string
- *   * @s: A pointer to an int that will be changed
- *    *
- *     * Return: void
- *      */
+ * rev_string-print a reverse string
+ * @s: A pointer to an int that will be changed
+ * Return: void
+ */
 
 void rev_string(char *s)
 {
-	char *star_c, *end_c, c;
-	int i, count;
-	int length = 0;
+char *star_c, *end_c, c;
+int i, count;
+int length = 0;
 
-	for (i = 0; s[i]; i++)
-	{
-		length++;
-	}
+for (i = 0; s[i]; i++)
+{
+length++;
+}
 
-	count = length;
+count = length;
 
-	star_c = s;
-	end_c = s;
-
-
-	for (i = 0; i < count - 1; i++)
-	{
-		end_c++;
-	}
+star_c = s;
+end_c = s;
 
 
-	for (i = 0; i < count / 2; i++)
-	{
+for (i = 0; i < count - 1; i++)
+{
+end_c++;
+}
 
 
-		c = *end_c;
-		*end_c = *star_c;
-		*star_c = c;
+for (i = 0; i < count / 2; i++)
+{
 
 
-		star_c++;
-		end_c--;
-	}
+c = *end_c;
+*end_c = *star_c;
+*star_c = c;
+
+
+star_c++;
+end_c--;
+}
 }
