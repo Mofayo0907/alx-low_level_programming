a1f3d935b5d46391d03876da51a1b1fbbcc5635c
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-20 01:28:03 +0100

    Update 7-puts_half.c

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,18 +1,30 @@
 #include "main.h"
+
 /**
- * _puts-prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
+ * puts_half-print every other character of a string
+ * @str: a pointer to an int that will be changed
+ *
+ * Description: if odd number of chars, print (length - 1) / 2
  */
 
+void puts_half(char *str)
 
-void _puts(char *str)
 {
-int i = 0;
-while (str[i])
+int i, last;
+
+i = 0;
+while (str[i] != '\0')
 {
-_putchar (str[i]);
 i++;
 }
-_putchar('\n');
+
+last = (i + 1) / 2;
+
+for (i = last; str[i]; i++)
+{
+_putchar (str[i]);
+}
+
+_putchar ('\n');
+
 }
