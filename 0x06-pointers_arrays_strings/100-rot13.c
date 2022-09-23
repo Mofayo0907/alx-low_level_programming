64e7857ea48fe0022df11c79801d80ee5da8d861
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-09-23 03:29:59 +0100

    pointers

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -7,26 +7,26 @@
 
 char *rot13(char *s)
 {
-	int i = 0, j = 0;
+int i = 0, j = 0;
 
-	char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
+char string_rot13[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 
-	char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+char string_alpha[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 
-	while (s[i] != '\0')
-	{
-		do{
-			if (s[i] == string_alpha[j])
-			{
-				s[i] = string_rot13[j];
-				break;
-			}
-			j++;
-		}
-		while (string_alpha[j] != '\0');
-	}
-	j = 0;
-	i++;
+while (s[i] != '\0')
+{
+do{
+if (s[i] == string_alpha[j])
+{
+s[i] = string_rot13[j];
+break;
+}
+j++;
+}
+while (string_alpha[j] != '\0');
+}
+j = 0;
+i++;
 
-	return (s);
+return (s);
 }
