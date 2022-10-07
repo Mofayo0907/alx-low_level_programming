7eed8f2c9b21871292d38a6834bde8923b6ca3b3
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-10-07 23:48:40 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,37 +12,37 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *str;
-	unsigned int s1_len, s2_len, i;
+char *str;
+unsigned int s1_len, s2_len, i;
 
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
-		;
+for (s1_len = 0; s1[s1_len] != '\0'; s1_len++)
+;
 
-	for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
-		;
+for (s2_len = 0; s2[s2_len] != '\0'; s2_len++)
+;
 
-	str = malloc(s1_len + s2_len + 1);
+str = malloc(s1_len + s2_len + 1);
 
 
-	if (n >= s2_len)
-		n = s2_len;
+if (n >= s2_len)
+n = s2_len;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		str[i] = s1[i];
+for (i = 0; s1[i] != '\0'; i++)
+str[i] = s1[i];
 
-	for (i = 0; i < n; i++)
-		str[s1_len + i] = s2[i];
+for (i = 0; i < n; i++)
+str[s1_len + i] = s2[i];
 
-	str[s1_len + n] = '\0';
+str[s1_len + n] = '\0';
 
-	if (str == NULL)
-		return (NULL);
+if (str == NULL)
+return (NULL);
 
-	return (str);
+return (str);
 }
