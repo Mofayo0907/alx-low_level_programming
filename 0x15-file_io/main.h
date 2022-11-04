21af679e49fe378575d841f544d3d3e683992313
Joy Ogunmodede <103996175+Mofayo0907@users.noreply.github.com>
2022-11-05 00:45:45 +0100

    Update main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,5 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
+
+/*
+ * File: main.h
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x15-file_io directory.
+ */
+
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
